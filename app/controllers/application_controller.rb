b4a4c32e89cb6b5b3263da9fb9022e4634c3fbcd
EC2 Default User <ksei@jcom.home.ne.jp>
2018-04-22 03:24:46 +0000

    improve the README file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  #def hello
-   # render html: "¡hola , mundo!"
-  # end
-  def tomo
-    render html: "ともちゃん大好き❤️"
+  def hello
+   render html: "Hello World !!!"
   end
+
 end
